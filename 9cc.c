8e76e88147c42785189d56026af01f1bd570b32f
Kippei Watanabe <k6play7210@gmail.com>
2020-02-28 22:13:57 -0800

    単項演算子対応

--- 9cc.c
+++ 9cc.c
@@ -162,6 +162,7 @@ Node *new_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 //パーサ
@@ -178,17 +179,28 @@ Node *expr() {
   }
 }
 
+//mul = unary("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_binary(ND_MUL, node, primary());
+      node = new_binary(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_binary(ND_DIV, node, primary());
+      node = new_binary(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+//unary = ("+" | "-")? unary
+//        | primary
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    return new_binary(ND_SUB, new_num(0), unary());
+  return primary();
 }
 
 Node *primary() {
