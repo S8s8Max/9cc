e04a7e5d4d556f8ce08598e0ac9c901086a28c81
Kippei Watanabe <k6play7210@gmail.com>
2020-03-29 21:51:45 -0700

    add unary & and *

--- codegen.c
+++ codegen.c
@@ -5,12 +5,19 @@ static char *argreg[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
 static int labelseq = 1;
 static char *funcname;
 
+static void gen(Node *node);
+
 static void gen_addr(Node *node) {
-  if (node->kind == ND_VAR) {
+  switch (node->kind) {
+  case ND_VAR:
     printf("  lea rax, [rbp-%d]\n", node->var->offset);
     printf("  push rax\n");
     return;
+  case ND_DEREF:
+    gen(node->lhs);
+    return;
   }
+
   error_tok(node->tok, "not an lvalue");
 }
 
@@ -44,6 +51,13 @@ static void gen(Node *node) {
     gen_addr(node->lhs);
     gen(node->rhs);
     store();
+    return;
+  case ND_ADDR:
+    gen_addr(node->lhs);
+    return;
+  case ND_DEREF:
+    gen(node->lhs);
+    load();
     return;
   case ND_IF: {
     int seq = labelseq++;
--- parse.c
+++ parse.c
@@ -269,6 +269,10 @@ static Node *unary(void) {
     return unary();
   if (tok = consume("-"))
     return new_binary(ND_SUB, new_num(0, tok), unary(), tok);
+  if (tok = consume("&"))
+    return new_unary(ND_ADDR, unary(), tok);
+  if (tok = consume("*"))
+    return new_unary(ND_DEREF, unary(), tok);
   return primary();
 }
 
