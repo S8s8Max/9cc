975115d497dc7c4751fcca1a2162f2f49caf48c7
Kippei Watanabe <k6play7210@gmail.com>
2020-04-07 17:27:53 -0700

    add [] operator

--- parse.c
+++ parse.c
@@ -69,6 +69,7 @@ static Node *relational(void);
 static Node *add(void);
 static Node *mul(void);
 static Node *unary(void);
+static Node *postfix(void);
 static Node *primary(void);
 
 Function *program(void) {
@@ -355,7 +356,19 @@ static Node *unary(void) {
     return new_unary(ND_ADDR, unary(), tok);
   if (tok = consume("*"))
     return new_unary(ND_DEREF, unary(), tok);
-  return primary();
+  return postfix();
+}
+
+static Node *postfix(void) {
+  Node *node = primary();
+  Token *tok;
+
+  while (tok = consume("[")) {
+    Node *exp = new_add(node, expr(), tok);
+    expect("]");
+    node = new_unary(ND_DEREF, exp, tok);
+  }
+  return node;
 }
 
 static Node *func_args(void) {
