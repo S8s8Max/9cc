412fd7f0b99e2a6a79d45496f450573ab3fdfe1d
Kippei Watanabe <k6play7210@gmail.com>
2020-04-23 17:50:47 +0900

    Make sizeof to accept not only an expression but also a typename

--- parse.c
+++ parse.c
@@ -151,7 +151,9 @@ static char *new_label(void) {
 static Function *function(void);
 static Type *basetype(bool *is_typedef);
 static Type *declarator(Type *ty, char **name);
+static Type *abstract_declarator(Type *ty);
 static Type *type_suffix(Type *ty);
+static Type *type_name(void);
 static Type *struct_decl(void);
 static Member *struct_member(void);
 static void global_var(void);
@@ -314,6 +316,21 @@ static Type *declarator(Type *ty, char **name) {
   return type_suffix;
 }
 
+//abstract-declarator = "*"* ("(" abstract-declarator ")")? type-suffix
+static Type *abstract_declarator(Type *ty) {
+  while (consume("*"))
+    ty = pointer_to(ty);
+  
+  if (consume("(")) {
+    Type *placeholder = calloc(1, sizeof(Type));
+    Type *new_ty = abstract_declarator(placeholder);
+    expect(")");
+    memcpy(placeholder, type_suffix(ty), sizeof(Type));
+    return new_ty;
+  }
+  return type_suffix(ty);
+}
+
 static Type *type_suffix(Type *ty) {
   if (!consume("["))
     return ty;
@@ -321,6 +338,13 @@ static Type *type_suffix(Type *ty) {
   expect("]");
   ty = type_suffix(ty);
   return array_of(ty, sz);
+}
+
+// type-name = basetype abstract-declarator type-suffix
+static Type *type_name(void) {
+  Type *ty = basetype(NULL);
+  ty = abstract_declarator(ty);
+  return type_suffix(ty);
 }
 
 static void push_tag_scope(Token *tok, Type *ty) {
@@ -575,7 +599,7 @@ static Node *stmt2(void) {
     node->body = head.next;
     return node;
   }
-  
+
   if (is_typename())
     return declaration();
 
@@ -793,6 +817,15 @@ static Node *primary(void) {
   }
   
   if (tok = consume("sizeof")) {
+    if (consume("(")) {
+      if (is_typename()) {
+        Type *ty = type_name();
+        expect(")");
+        return new_num(ty->size, tok);
+      }
+      token = tok->next;
+    }
+
     Node *node = unary();
     add_type(node);
     return new_num(node->ty->size, tok);
